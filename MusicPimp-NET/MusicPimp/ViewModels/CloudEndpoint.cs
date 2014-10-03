048ba4cd8e550662fd7aef6b20af55b10406ecc9
malliina <malliina123@gmail.com>
2014-10-03 21:50:37 +0300

    added files

---
+++ MusicPimp-NET/MusicPimp/ViewModels/CloudEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Mle.MusicPimp.ViewModels {
+    public class CloudEndpoint : MusicEndpoint {
+        public static readonly string SERVER = "cloud.musicpimp.org";
+        //public static readonly string SERVER = "localhost";
+        public static readonly int PORT = 443;
+        //public static readonly int PORT = 9000;
+        public static readonly Protocols PROTOCOL = Protocols.https;
+        //public static readonly Protocols PROTOCOL = Protocols.http;
+        public CloudEndpoint() {
+            Protocol = PROTOCOL;
+            Server = SERVER;
+            Port = PORT;
+            EndpointType = EndpointTypes.PimpCloud;
+        }
+        public CloudEndpoint(string name, string pimpID, string username, string password)
+            : this() {
+            Name = name;
+            CloudServerID = pimpID;
+            Username = username;
+            Password = password;
+        }
+    }
+}
